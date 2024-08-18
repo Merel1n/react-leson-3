ca53364873868346685519ef9df9742a64d1a259
svitlana_havrylets <svitlana.havrylets@gmail.com>
2024-08-18 11:17:49 +0200

    task-1

--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,10 +1,34 @@
-import { Container, Heading, Section } from 'components';
+import { Container, SearchForm, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/countryApi';
 
 export const SearchCountry = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [countries, setCountries] = useState([]);
+  const getRegion = region => {
+    setSearchParams({ name: region });
+  };
+  const name = searchParams.get('name');
+  console.log(name);
+
+  useEffect(() => {
+    if (name === null) return;
+    const asyncWrapper = async () => {
+      try {
+        const data = await fetchByRegion(name);
+        setCountries(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    asyncWrapper();
+  }, [name]);
+
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <SearchForm onSubmit={getRegion} />
       </Container>
     </Section>
   );
